a36fca849c45e913dc59017b308f004f505a8d90
Dhruv Doshi <dhruv@macbookair.home>
2024-10-06 18:30:21 -0400

    resolved uslint error

--- src/components/DraggableSlider.js
+++ src/components/DraggableSlider.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 
 const DraggableSlider = ({ label, value, setValue, min, max, step, currencySymbol, currentYear, showYear }) => {
   const [isDragging, setIsDragging] = useState(false);
